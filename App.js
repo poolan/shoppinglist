d1bc6084a8ab98d7463ac8a6de19a81ddd0d1ec3
raul fernandez <raulpoolan@gmail.com>
2022-09-13 00:19:08 +0300

    done

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [item, setItem] = React.useState('');
+  const [data, setData] = React.useState([]);
+
+  const addButton = () => {
+    setData([...data, { key: `${item}` }]);
+    setItem('')
+  };
+
+  const clearButton = () => {
+    setData('')
+    setItem('')
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <TextInput
+          value={item}
+          onChangeText={(txt) => setItem(txt)}
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="ADD" onPress={addButton} />
+        <Button title="CLEAR" onPress={clearButton} />
+      </View>
+      <View style={styles.historyContainer}>
+        <Text style={{color: "darkblue", fontSize:22}}> Shopping List</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Text>{item.key}</Text>}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +53,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 10,
+    
+  },
+
+  buttonContainer: {
+    marginBottom: 20,
+    marginTop:0,
+    width: 150,
+    backgroundColor: "#0466C7",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  input: {
+    fontSize: 20,
+    width: 170,
+    borderColor: "gray",
+    borderWidth: 1,
+  
+  },
+  historyContainer: {
+    flex: 2,
+    width: 150,
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
